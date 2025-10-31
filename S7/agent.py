e07842aef9ae9033d0d3e9f3f5c8b22d7d8b830b
sujitojha1 <sujit.ojha@gmail.com>
2025-11-01 00:59:26 +0530

    minor changes

--- S7/agent.py
+++ S7/agent.py
@@ -27,7 +27,7 @@ async def main(user_input: str):
         server_params = StdioServerParameters(
             command="python",
             args=["example3.py"],
-            cwd="I:/TSAI/2025/EAG/Session 7/S7"
+            cwd="/Users/payalchakraborty/Dev/EAG2/gemini-blog-buddy/S7"
         )
 
         try:
